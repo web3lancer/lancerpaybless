416c4989a16c8f02026813e58287c2034e9376d2
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-06-10 05:51:42 +0000

    apparently the comments were messing with my shyt

--- src/logger.ts
+++
@@ -1,9 +0,0 @@
-// Logger implementation for Bless Network environment
-const logger = {
-  info: (message: string, ...args: any[]) => console.log(`[BLESS-INFO] ${message}`, ...args),
-  error: (message: string, ...args: any[]) => console.error(`[BLESS-ERROR] ${message}`, ...args),
-  warn: (message: string, ...args: any[]) => console.warn(`[BLESS-WARN] ${message}`, ...args),
-  debug: (message: string, ...args: any[]) => console.log(`[BLESS-DEBUG] ${message}`, ...args)
-};
-
-export default logger;
